0855d67d1725359fa139584b0f49743c642158fc
marineolvr <87575559+marineolvr@users.noreply.github.com>
2021-10-05 20:28:44 -0500

    Update C_youtube.py

--- C_youtube.py
+++ C_youtube.py
@@ -11,14 +11,19 @@ driver.get("https://chykalophia.com/")
 
 driver.maximize_window()
 
-#clicking on the button to go to youtube
-youtube_btn = driver.find_element_by_xpath('//div[2]//span[5]/a')
-youtube_btn.click()
 
 #webdriverwait
 youtube_btn = driver.find_element_by_xpath('//div[2]//span[5]/a')
 youtube_btn.click()
 wd_wait = WebDriverWait(driver, 10)
 youtube_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "//div[2]//span[5]/a")))
+
+element = driver.find_element_by_xpath('//div[2]//span[5]/a')
+if element.is_displayed():
+    print("Passed")
+else:
+    print("Failed")
+
+
 
 driver.quit()
