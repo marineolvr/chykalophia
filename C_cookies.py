4cb100505cc7e0900a336cf0c44184bec2ee1571
marineolvr <87575559+marineolvr@users.noreply.github.com>
2021-10-05 20:29:09 -0500

    Update C_cookies.py

--- C_cookies.py
+++ C_cookies.py
@@ -6,19 +6,21 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 #google chrome webdriver
+
 driver = webdriver.Chrome()
 driver.get("https://chykalophia.com/")
 
 driver.maximize_window()
 
-#clicking on the button to go to cookies policy in footer
+#clicking on the button to go to linkedin
 cookies_btn = driver.find_element_by_xpath('//div[2]//p/a[2]')
 cookies_btn.click()
 
-#webdriverwait
-cookies_btn = driver.find_element_by_xpath('//div[2]//p/a[2]')
-cookies_btn.click()
-wd_wait = WebDriverWait(driver, 10)
-cookies_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "//div[2]//p/a[2]")))
+element = driver.find_element_by_xpath('//div[2]//p/a[2]')
+if element.is_displayed():
+    print("Passed")
+else:
+    print("Failed")
+
 
 driver.quit()
