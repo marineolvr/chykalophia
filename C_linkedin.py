02f62e4653d31b7f36a236d8a070be9adc5f5312
marineolvr <87575559+marineolvr@users.noreply.github.com>
2021-10-05 20:26:58 -0500

    Update C_linkedin.py

--- C_linkedin.py
+++ C_linkedin.py
@@ -11,16 +11,18 @@ driver.get("https://chykalophia.com/")
 
 driver.maximize_window()
 
-#webdriverwait
-#linkedin_btn = driver.find_element_by_xpath('//div[2]//span[1]/a')
-#linkedin_btn.click()
-wd_wait = WebDriverWait(driver, 10)
-linkedin_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "//div[2]//span[1]/a")))
-
-#clicking on the button to go to linkedin
+#clicking on the button to go to instagram
 linkedin_btn = driver.find_element_by_xpath('//div[2]//span[1]/a')
 linkedin_btn.click()
 
+#wd_wait = WebDriverWait(driver, 10)
+#linkedin_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "//div[2]//span[4]/a")))
+
+element = driver.find_element_by_xpath('//div[2]//span[1]/a')
+if element.is_displayed():
+    print("Passed")
+else:
+    print("Failed")
+
+
 driver.quit()
-
-
