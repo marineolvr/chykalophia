8eb409c6d32cd7e35cdfc9ebcd1b8bc8a1fe6da8
marineolvr <87575559+marineolvr@users.noreply.github.com>
2021-10-05 20:24:49 -0500

    Update C_Behance.py

--- C_Behance.py
+++ C_Behance.py
@@ -1,3 +1,4 @@
+
 from selenium import webdriver
 
 #imports for selenium WebDriverWait
@@ -6,19 +7,23 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 #google chrome webdriver
+
 driver = webdriver.Chrome()
 driver.get("https://chykalophia.com/")
 
 driver.maximize_window()
 
-#clicking on the button to go to behance
-behance_btn = driver.find_element_by_xpath('//div[2]//span[6]/a')
+#clicking on the button to go to linkedin
+behance_btn = driver.find_element_by_xpath('//div[2]//span[7]/a')
 behance_btn.click()
+#wd_wait = WebDriverWait(driver, 10)
+#behance_btn = WebDriverWait.until(EC.visibility_of_element_located((By.XPATH, '//div[2]/ul[1]/li[1]/a/div')))
 
-#webdriverwait
-behance_btn = driver.find_element_by_xpath('//div[2]//span[6]/a')
-behance_btn.click()
-wd_wait = WebDriverWait(driver, 10)
-behance_btn = wd_wait.until(EC.visibility_of_element_located((By.XPATH, "//div[2]//span[6]/a")))
+
+element = driver.find_element_by_xpath("//div[2]//span[7]/a")
+if element.is_displayed():
+    print("Passed")
+else:
+    print("Failed")
 
 driver.quit()
